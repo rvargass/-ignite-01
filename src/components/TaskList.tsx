6e0a033314ee59e4572de196e1526367853e470e
rvargass <62122622+rvargass@users.noreply.github.com>
2021-08-13 20:10:39 -0300

    add mensagem de alerta para campo vazio

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,7 +16,7 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
-    if (!newTaskTitle) return;
+    if (!newTaskTitle) return alert("O campo 'Adicionar novo todo' não pode estar vazio!");
 
     const newTask = {
       id: Math.random(),
